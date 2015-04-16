eb79ec28bc5f54b5c0d48bde5a500987e8e2c1ad
cristianbarri <cristian.barrientos1401@gmail.com>
2015-04-16 12:10:21 +0200

    Abre Perfil tras login

--- app/src/main/java/com/playit/playit/MainActivity.java
+++ app/src/main/java/com/playit/playit/MainActivity.java
@@ -1,17 +1,47 @@
 package com.playit.playit;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
 
+
+    //TextView t;
+    private Button b;
+    private EditText user, pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        user = (EditText)findViewById(R.id.editText1);
+        pass = (EditText)findViewById(R.id.editText2);
+
+        b = (Button)findViewById(R.id.button1);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!user.getText().toString().equals("") && !pass.getText().toString().equals("")) {
+                    Intent i = new Intent(getApplicationContext(), MainActivity2.class);
+                    startActivity(i);
+                }
+                else {
+                    Toast t = Toast.makeText(getApplicationContext(), "Bad Log In", Toast.LENGTH_SHORT);
+                    t.show();
+                }
+            }
+        });
+        /*t = (TextView)findViewById(R.id.textView1);
+        t.setText("Hola");*/
     }
 
 
