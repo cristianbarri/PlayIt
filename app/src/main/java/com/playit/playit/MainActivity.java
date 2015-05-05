d145ce6a62427967848b2b00f9bc2abf2594cb8c
marcfernandezantolin <marc_7_5@hotmail.com>
2015-05-05 22:12:54 +0200

    mucha cosa, incluido el nuevo login y signup

--- app/src/main/java/com/playit/playit/MainActivity.java
+++ app/src/main/java/com/playit/playit/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.os.AsyncTask;
 import android.os.StrictMode;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -14,39 +13,31 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.playit.playit.UtilsHTTP.AsyncTaskLogin;
 import com.playit.playit.UtilsHTTP.CustomHttpClient;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
+
+//importar para parse.com
 
 
 public class MainActivity extends ActionBarActivity {
 
 
     //TextView t;
-    private Button b;
-    private EditText user, pass;
+    private Button b, b1;
+    private EditText user, pass, edit_passconf, edit_email;
+    private boolean showSignUp = false;
+    private TextView text_passconf, text_email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,57 +49,65 @@ public class MainActivity extends ActionBarActivity {
 
         user = (EditText)findViewById(R.id.editText1);
         pass = (EditText)findViewById(R.id.editText2);
+        edit_passconf = (EditText)findViewById(R.id.editText);
+        edit_email = (EditText)findViewById(R.id.editText3);
+        text_email = (TextView)findViewById(R.id.textView6);
+        text_passconf = (TextView)findViewById(R.id.textView5);
+
+
+        //Invisibles las cosas para SIGNUP
+        edit_passconf.setVisibility(View.GONE);
+        edit_email.setVisibility(View.GONE);
+        text_passconf.setVisibility(View.GONE);
+        text_email.setVisibility(View.GONE);
+
 
         b = (Button)findViewById(R.id.button1);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String uname = user.getText().toString();
-                String pwd = pass.getText().toString();
+                if (!showSignUp)
+                    doLogin(user.getText().toString(), pass.getText().toString());
 
-                //AsyncTaskLogin task = new AsyncTaskLogin();
-                //task.execute(new String[]{uname, pwd});
-                doLogin(user.getText().toString(), pass.getText().toString());
+                else {
+                    //Oculto los campos
+                    edit_passconf.setVisibility(View.GONE);
+                    edit_email.setVisibility(View.GONE);
+                    text_passconf.setVisibility(View.GONE);
+                    text_email.setVisibility(View.GONE);
+                    RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams)b1.getLayoutParams();
+                    params.addRule(RelativeLayout.ALIGN_TOP, R.id.editText2);
+
+                    b1.setLayoutParams(params); //causes layout update
+                    showSignUp = false;
+                }
             }
         });
+
+        b1 = (Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (showSignUp) {
+                    if (android.util.Patterns.EMAIL_ADDRESS.matcher(edit_email.getText().toString()).matches())
+                        doSignup(user.getText().toString(), edit_email.getText().toString(), pass.getText().toString(), edit_passconf.getText().toString());
+                    else Toast.makeText(getApplicationContext(), "This isn't a valid email", Toast.LENGTH_LONG).show();
+                }else {
+                    //Enseño los campos
+                    edit_passconf.setVisibility(View.VISIBLE);
+                    edit_email.setVisibility(View.VISIBLE);
+                    text_passconf.setVisibility(View.VISIBLE);
+                    text_email.setVisibility(View.VISIBLE);
+                    RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams)b1.getLayoutParams();
+                    params.addRule(RelativeLayout.ALIGN_TOP, R.id.editText3);
+
+                    b1.setLayoutParams(params); //causes layout update
+                    showSignUp = true;
+                }
+            }
+        });
+
     }
-
-    private class validateUserTask extends AsyncTask<String, Void, String> {
-        String response = null;
-        @Override
-        protected String doInBackground(String... params) {
-            // TODO Auto-generated method stub
-            ArrayList<NameValuePair> postParameters = new ArrayList<NameValuePair>();
-            postParameters.add(new BasicNameValuePair("username", params[0] ));
-            postParameters.add(new BasicNameValuePair("password", params[1] ));
-            String res = null;
-            try {
-                response = CustomHttpClient.executeHttpPost("http://www.marcfernandezantolin.com/test.php", postParameters);
-                res=response.toString();
-                JSONObject jObject = new JSONObject(res);
-                res = jObject.getString("login");
-
-            }
-            catch (Exception e) {
-                //txt_Error.setText(e.toString());
-            }
-            return res;
-        }//close doInBackground
-
-        @Override
-        protected void onPostExecute(String result) {
-            if(result.equals("1")){
-                //navigate to Main Menu
-                Intent i = new Intent(MainActivity.this, ProfileSwipe.class);
-                startActivity(i);
-            }
-            else{
-                //txt_Error.setText("Sorry!! Incorrect Username or Password");
-            }
-        }//close onPostExecute
-    }// close validateUserTask
-//close LoginActivity
-
 
     protected void doLogin(String user, String pass) {
         ConnectivityManager connMgr = (ConnectivityManager)
@@ -129,83 +128,76 @@ public class MainActivity extends ActionBarActivity {
             postParameters.add(new BasicNameValuePair("password", pass ));
             String response;
             try {
-                response = CustomHttpClient.executeHttpPost("http://www.marcfernandezantolin.com/test.php", postParameters);
+                response = CustomHttpClient.executeHttpPost("http://46.101.139.161/bdapi/login.php", postParameters);
                 Log.i("response","feta");
-                /*JSONObject jObject = new JSONObject(response);
+                JSONObject jObject = new JSONObject(response);
                 Log.i("json","creat");
                 response = jObject.getString("login");
                 Log.i("json","parsejat");
-                */Log.i("response",response);
-                if (response.contains("1")/*.equals("1")*/) {
+                Log.i("response",response);
+                if (response.equals("1")) {
                     Intent i = new Intent(this, ProfileSwipe.class);
                     startActivity(i);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Invalid user or password", Toast.LENGTH_LONG).show();
                 }
             } catch (Exception e) {
                 Log.i("peta", "merdaca");
             }
 
-            // Execute the request
-
-            /*try {
-                response = httpclient.execute(httpget);
-                // Examine the response status
-                Log.i("Praeda", response.getStatusLine().toString());
-
-                // Get hold of the response entity
-                HttpEntity entity = response.getEntity();
-                // If the response does not enclose an entity, there is no need
-                // to worry about connection release
-
-                if (entity != null) {
-
-                    // A Simple JSON Response Read
-                    InputStream instream = entity.getContent();
-                    String result = convertStreamToString(instream);
-                    // now you have the string representation of the HTML request
-                    instream.close();
-                    JSONObject jObject = new JSONObject(result);
-                    String veredict = jObject.getString("login");
-                    Toast.makeText(getApplicationContext(), veredict,
-                            Toast.LENGTH_LONG).show();
-                    if (veredict.equals("1")) {
-                        Intent i = new Intent(this, ProfileSwipe.class);
-                        startActivity(i);
-                    }
-                }
-
-
-            } catch (Exception e) {
-                Log.i("peta", "merdaca");
-            }*/
 
         }
     }
 
-    private static String convertStreamToString(InputStream is) {
-    /*
-     * To convert the InputStream to String we use the BufferedReader.readLine()
-     * method. We iterate until the BufferedReader return null which means
-     * there's no more data to read. Each line will appended to a StringBuilder
-     * and returned as String.
-     */
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        StringBuilder sb = new StringBuilder();
 
-        String line = null;
-        try {
-            while ((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                is.close();
-            } catch (IOException e) {
-                e.printStackTrace();
+    protected void doSignup(String user, String email, String pass, String pass2) {
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        if (networkInfo == null || !networkInfo.isConnected()) {
+            Log.i("info", "noNetwork");
+        } else {
+            if (user.equals("") || email.equals("") || pass.equals("") || pass2.equals("")){
+                Toast.makeText(getApplicationContext(), "Please fill in all the fields", Toast.LENGTH_LONG).show();
+            } else {
+                ArrayList<NameValuePair> postParameters = new ArrayList<NameValuePair>();
+                postParameters.add(new BasicNameValuePair("name", user));
+                postParameters.add(new BasicNameValuePair("email", email));
+                Log.i("passwords:", pass + " " + pass2);
+                if (pass.equals(pass2)) {
+                    postParameters.add(new BasicNameValuePair("password", pass));
+
+
+                    String response;
+                    try {
+                        response = CustomHttpClient.executeHttpPost("http://46.101.139.161/bdapi/signup.php", postParameters);
+
+                        if (!response.contains("user already exists") && !response.contains("0")) {
+                            //Ya estoy totalmente registrado pues escondo los campos
+                            edit_passconf.setVisibility(View.GONE);
+                            edit_email.setVisibility(View.GONE);
+                            text_passconf.setVisibility(View.GONE);
+                            text_email.setVisibility(View.GONE);
+                            RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) b1.getLayoutParams();
+                            params.addRule(RelativeLayout.ALIGN_TOP, R.id.editText2);
+
+                            b1.setLayoutParams(params); //causes layout update
+                            showSignUp = false;
+                        } else if (response.contains("user already exists")){
+                            Toast.makeText(getApplicationContext(), "This user already exists", Toast.LENGTH_LONG).show();
+                        }else {
+                            Toast.makeText(getApplicationContext(), "There was an error signing up", Toast.LENGTH_LONG).show();
+                        }
+                    } catch (Exception e) {
+                        Log.i("peta", "merdaca");
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "Passwords don't match", Toast.LENGTH_LONG).show();
+                }
             }
         }
-        return sb.toString();
+
     }
 
     @Override
